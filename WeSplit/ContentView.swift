6780b7b62884736b44ddfcc3ec182252940b4c91
Diogo Gaspar <ghostlabz@gmail.com>
2021-02-17 02:31:24 -0300

    feat: done with textfield

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,20 +10,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(numberOfPeople) ?? 2
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
 
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-        
+
         return amountPerPerson
     }
     
@@ -36,6 +36,8 @@ struct ContentView: View {
         return totalToPay
     }
     
+//     Opa
+    
     var body: some View {
         NavigationView {
             Form {
@@ -43,11 +45,13 @@ struct ContentView: View {
                     TextField("Amount: ", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+                    TextField("Number of people (default: 2)", text: $numberOfPeople)
+                    
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
                 }
                 
                 Section(header: Text("How much tip do you want to leave?")
